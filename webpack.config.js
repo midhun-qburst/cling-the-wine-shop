d96d226f0559109856da2cf3f968e4064ec1b861
mindphreaked <midhuns@qburst.com>
2018-01-22 14:47:21 +0530

    initial configuration - frontend

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: './build/bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  // Emit source maps so we can debug our code in the browser
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: 'css-loader',
+        }),
+      }
+    ]
+  },
+  devServer: {
+    inline: true,
+    port: 4040,
+    
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: 'index.html'
+    }),
+  //  new ExtractTextPlugin('style.bundle.css')
+  ]
+};
